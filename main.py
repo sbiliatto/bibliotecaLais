c9e6edac50922223738c9774144d81bd9238536d
sbiliatto <sophia.biliattoo@gmail.com>
2025-09-12 11:18:34 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, flash, url_for
 import fdb
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
 
 host = 'localhost'
 database = r'C:\Users\Aluno\Desktop\BANCO.FDB'
@@ -18,6 +19,118 @@ def index():
     cursor.close()
 
     return render_template('livros.html', livros=livros)
+
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo= 'Novo Livro')
+
+
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    titulo = request.form['titulo']
+    autor = request.form['autor']
+    ano_publicado = request.form['ano_publicado']
+
+    cursor = con.cursor()
+    try:
+        cursor.execute('SELECT 1 FROM lIVRO WHERE LIVRO.TITULO = ?', (titulo,))
+        if cursor.fetchone():
+            flash('Esse livro já está cadastrado')
+            return redirect(url_for('novo'))
+        cursor.execute("insert into livro(titulo, autor, ano_publicado) values (?,?,?)",
+                       (titulo, autor, ano_publicado))
+        con.commit()
+    finally:
+        cursor.close()
+    flash('O livro foi cadastrado com sucesso')
+    return redirect(url_for('index'))
+
+@app.route('/atualizar')
+def atualizar():
+    return render_template('editar.html', titulo='Editar livro')
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    cursor = con.cursor()
+    cursor.execute('select id_livro, titulo, autor, ano_publicado from livro where id_livro =?', (id,))
+    livro = cursor.fetchone()
+
+    if not livro:
+        cursor.close()
+        flash("Livro não foi encontrado")
+        return redirect(url_for('index'))
+
+
+    if request.method == 'POST':
+        titulo = request.form['titulo']
+        autor = request.form['autor']
+        ano_publicado = request.form['ano_publicacao']
+
+        cursor.execute("update livro set titulo = ?, autor = ?, ano_publicado = ? where id_livro = ?" ,
+        (titulo, autor, ano_publicado, id))
+
+        con.commit()
+        flash("Livro atualizado com sucesso")
+        return  redirect(url_for('index'))
+    cursor.close()
+    return render_template('editar.html', livro=livro, titulo= 'Editar livro')
+
+@app.route('/deletar/<int:id>', methods=('POST',))
+def deletar(id):
+        cursor = con.cursor()  # Abre o cursor
+
+        try:
+            cursor.execute('DELETE FROM livro WHERE id_livro = ?', (id,))
+            con.commit()  # Salva as alterações no banco de dados
+            flash('Livro excluído com sucesso!', 'success')  # Mensagem de sucesso
+        except Exception as e:
+            con.rollback()  # Reverte as alterações em caso de erro
+            flash('Erro ao excluir o livro.', 'error')  # Mensagem de erro
+        finally:
+            cursor.close()  # Fecha o cursor independentemente do resultado
+
+        return redirect(url_for('index'))  # Redireciona para a página principal
+
+
+@app.route('/lista_usuario')
+def lista_usuario():
+    cursor = con.cursor()
+    cursor.execute("SELECT id, nome, email, senha FROM usuarios")
+    usuarios = cursor.fetchall()
+    cursor.close()
+    return render_template('usuario.html', usuarios=usuarios)
+
+
+@app.route('/novo_usuario')
+def novo_usuario():
+    return render_template('novousuario.html', titulo='Novo Usuário')
+
+
+@app.route('/criar_usuario', methods=['POST'])
+def criar_usuario():
+    nome = request.form['nome']
+    email = request.form['email']
+    senha = request.form['senha']
+
+    cursor = con.cursor()
+    try:
+        cursor.execute("SELECT 1 FROM usuarios WHERE email = ?", (email,))
+        if cursor.fetchone():
+            flash('Esse e-mail já está cadastrado!', 'error')
+            return redirect(url_for('novo_usuario'))
+
+        cursor.execute(
+            "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)",
+            (nome, email, senha)
+        )
+        con.commit()
+    finally:
+        cursor.close()
+
+    flash('Usuário cadastrado com sucesso!', 'success')
+    return redirect(url_for('lista_usuario'))
 
 if __name__ == '__main__':
     app.run(debug=True)
